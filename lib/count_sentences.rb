9135e081824c586b053084b460d5504d8dd504fc
Toni-Ty <antonique2479@yahoo.com>
2019-03-10 01:26:10 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -42,38 +42,38 @@ end
 
 # require 'pry'
 # class Song
-# 
+#
 # attr_accessor :name, :artist, :genre
-# 
+#
 # @@count = 0
 # @@artists = []
 # @@genres = []
-# 
+#
 # def initialize (name, artist, genre)
 #   @name = name
 #   @genre = genre
 #   @artist = artist
-# 
-# 
+#
+#
 # @@count +=1
 # @@genres << genre
 # @@artists << artist
 # end
-# 
-# 
+#
+#
 # def self.count
 #   @@count
 #   end
-# 
+#
 # def self.genres
 #   @@genres.uniq
 #   end
-# 
-# 
+#
+#
 # def self.artists
 #   @@artists.uniq
 # end
-# 
+#
 # def self.genre_count
 #   genre_count = {}
 #   @@genres.each do |genre|
@@ -85,7 +85,7 @@ end
 #     end
 #     genre_count
 # end
-# 
+#
 # def self.artist_count
 #   artist_count = {}
 #   @@artists.each do |artist|
