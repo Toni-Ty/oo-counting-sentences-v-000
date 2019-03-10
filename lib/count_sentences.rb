093033db37475f55473f8d9d12b2d61e350a8776
Toni-Ty <antonique2479@yahoo.com>
2019-03-10 01:40:20 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,6 +27,7 @@ end
   end
 
   def count_sentences
+    binding.pry
       self.split(/\.|\?|\!/).size #how does delete_if function here?
       # below is also correct
       # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
