146a5fb8b5df24501fb3888f521ea5c48bc1dcd1
Toni-Ty <antonique2479@yahoo.com>
2019-03-10 02:24:54 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,19 +27,19 @@ end
   end
 
   def count_sentences
-    # There are two exclamation point characters in the middle of this set of 3 sentences 
-    # ("This is too!!") so it splits the sentence right between the 2 exclam. points, 
-    # leaving an empty string that the delete_if method deletes, via the code between the 
+    # There are two exclamation point characters in the middle of this set of 3 sentences
+    # ("This is too!!") so it splits the sentence right between the 2 exclam. points,
+    # leaving an empty string that the delete_if method deletes, via the code between the
     # curly braces.  Last it returns the size of the sentence string.
-    self.split(/[.!?]/).delete_if {|x| x.empty?}.size 
+    self.split(/[.!?]/).delete_if {|x| x.empty?}.size
 
-      # self.split(/[.!?]/).reject {|x| x.empty?}.size 
+      # self.split(/[.!?]/).reject {|x| x.empty?}.size
       # above and below are also correct
-      # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size 
-      #For the code above, it deletes sentences that have a character size of less than 
+      # self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+      #For the code above, it deletes sentences that have a character size of less than
       # 2 characters and then returns the size of the sentence string.
-      # It appears that a higher number than 2 could've been chosen. 
-      # 
+      # It appears that a higher number than 2 could've been chosen.
+      #
 
 
 
@@ -48,5 +48,3 @@ end
 
   end
 end
-
-
